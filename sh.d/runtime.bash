68ff9888c102aec3d818f98fdf1c03466b4bbc66
Sidney San Martín <sidney@okcupid.com>
2014-08-22 15:02:56 -0400

    Show execution time after each command

---
+++ sh.d/runtime.bash
@@ -0,0 +1,28 @@
+mstime () { echo "$(($(date +%s%N)/1000000))"; }
+
+preexec () {
+    if [[ -z $1 ]]; then
+        unset LAST_START_TIME
+        return
+    fi
+    LAST_START_TIME="`mstime`";
+}
+
+precmd () {
+    [[ -z $LAST_START_TIME ]] && return
+    echo
+    echo -e "\e[90m$(($(mstime)-$LAST_START_TIME))\e[0m"
+    unset LAST_START_TIME
+}
+
+PROMPT_COMMAND="precmd"
+
+if [[ -n $BASH_VERSION ]]; then
+    preexec_invoke_exec () {
+        [ -n "$COMP_LINE" ] && return  # do nothing if completing
+        [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
+        local this_command=`HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//"`;
+        preexec "$this_command"
+    }
+    trap 'preexec_invoke_exec' DEBUG
+fi
