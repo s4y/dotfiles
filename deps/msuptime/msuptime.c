c1b5a1cbd9fad3fde8468f4e13f7ad108437d8a2
Sidney San Martín <sidney@s4y.us>
2017-06-04 16:00:35 -0400

    Linux support! Probably!

--- deps/msuptime/msuptime.c
+++ deps/msuptime/msuptime.c
@@ -4,7 +4,13 @@
 
 int main() {
 	struct timespec time;
-	if (clock_gettime(CLOCK_UPTIME_RAW, &time) != 0)
+	if (clock_gettime(
+#ifdef CLOCK_UPTIME_RAW
+	CLOCK_UPTIME_RAW,
+#else
+	CLOCK_MONOTONIC,
+#endif
+	&time) != 0)
 		return errno;
 	printf("%ld\n", (time.tv_sec * 1000) + (time.tv_nsec / 1000000));
 	return 0;
