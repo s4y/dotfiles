f7e3a531b7aeb80ae4d6693fbc49fc56e2a2f0c5
Sidney San Martín <sidney@s4y.us>
2018-05-24 16:22:30 -0400

    Fix runtime.bash printing at the wrong time in pipelines.

--- sh.d/runtime.bash
+++ sh.d/runtime.bash
@@ -23,6 +23,7 @@ if [[ -n $BASH_VERSION ]]; then
         [ -n "$COMP_LINE" ] && return  # do nothing if completing
         [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
         local this_command=`HISTTIMEFORMAT= history 1 | sed -e "s/^[ ]*[0-9]*[ ]*//"`;
+        [[ $BASH_COMMAND != $this_command ]] && return
         preexec "$this_command"
     }
     trap 'preexec_invoke_exec' DEBUG
